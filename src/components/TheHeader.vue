8415c5d8969dbde24d1606cd22d26254626ae34b
oziel <oziel.silva222@gmail.com>
2021-12-08 20:57:58 -0300

    configurando a rota do usuario e login

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,14 +4,22 @@
       <router-link to="/" class="logo">
         <img src="@/assets/ranek.svg" alt="Ranek">
       </router-link>
-      <router-link class="btn" to="/login">Vender / Login</router-link>
+      <router-link class="btn" v-if="$store.state.login" to="/usuario">
+        {{nome}}
+      </router-link>
+      <router-link v-else class="btn" to="/login">Vender / Login</router-link>
     </nav>
   </header>
 </template>
 
 <script>
 export default {
-  name: "TheHeader"
+  name: "TheHeader",
+  computed: {
+    nome() {
+      return this.$store.state.usuario.nome.replace(/ .*/, "")
+    }
+  }
 }
 </script>
 
---
+++ src/views/usuario/Usuario.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Página do usuario {{$store.state.usuario}}</p>
+</template>
+
+<script>
+export default {
+  name: "Usuario"
+}
+</script>
+
+<style>
+
+</style>
