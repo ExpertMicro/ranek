6642a21f87976febcd03b0eae6d5dc01065c7952
oziel <oziel.silva222@gmail.com>
2021-12-08 14:59:51 -0300

    configurando o filtro dos preços

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,26 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "../views/Home.vue"
+import Produto from "../views/Produto.vue"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
+  },
+  {
+    path: "/produto/:id",
+    name: "produto",
+    component: Produto,
+    props: true
   },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
   scrollBehavior() {
