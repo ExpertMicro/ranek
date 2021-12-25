783434e5f349080c349b1109e9c12530824d590b
oziel <oziel.silva222@gmail.com>
2021-12-25 18:11:05 -0300

    tratando os erros

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -1,6 +1,7 @@
 <template>
   <section>
     <h2>Crie a sua conta</h2>
+    <ErroNotificacao :erros="erros" />
     <transition mode="out-in">
       <button v-if="!criar" class="btn" @click="criar = true">Criar Conta</button>
       <UsuarioForm v-else>
@@ -21,18 +22,20 @@ export default {
   },
   data() {
     return {
-      criar: false
+      criar: false,
+      erros: []
     };
   },
   methods: {
     async criarUsuario() {
+      this.erros = [];
       try {
         await this.$store.dispatch("criarUsuario", this.$store.state.usuario);
         await this.$store.dispatch("logarUsuario", this.$store.state.usuario);
         await this.$store.dispatch("getUsuario");
         this.$router.push({ name: "usuario" })
       } catch(error) {
-        console.log(error)
+        this.erros.push(error.response.data.message)
       }
     }
   }
